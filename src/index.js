d5b3a38ac69dc0db9a5c9e8a12a669038d66f9b9
zymfe <1047832475@qq.com>
2020-02-09 18:12:44 +0800

    test

--- src/index.js
+++ src/index.js
@@ -3,14 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-class TestApp extends React.Component {
-  render () {
-    return <div>TestApp</div>
-  }
-}
-
-// ReactDOM.render(<App key="App" />, document.getElementById('root1'));
-
-// ReactDOM.createRoot(document.getElementById('root')).render(App)
-
-ReactDOM.createPortal('#root1')
+ReactDOM.render(<App />, document.getElementById('root1'));
